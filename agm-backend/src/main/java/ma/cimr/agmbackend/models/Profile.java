e29ccbcac309e08f3a90cdb9db26e90fdbf3e64b
devbymed <devbymed@gmail.com>
2024-04-12 23:37:13 +0000

    Update code changes

--- agm-backend/src/main/java/ma/cimr/agmbackend/models/Profile.java
+++ agm-backend/src/main/java/ma/cimr/agmbackend/models/Profile.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,7 +30,7 @@ public class Profile {
 	@NotBlank(message = "Le nom du profil est obligatoire")
 	private String name;
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "profile")
+	@OneToMany(mappedBy = "profile")
 	private List<User> users;
 
 	@CreationTimestamp
