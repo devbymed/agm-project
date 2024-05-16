32e28ffafb7d672b150a791fc10f7a2e233e54cc
devbymed <devbymed@gmail.com>
2024-05-16 23:57:37 +0000

    Update code changes

--- agm-frontend/src/app/app.component.ts
+++ agm-frontend/src/app/app.component.ts
@@ -7,21 +7,7 @@ import { initFlowbite } from 'flowbite';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent],
-  template: ` @if (isAuthenticated) {
-      <app-navbar />
-      <div class="flex overflow-hidden bg-gray-50 pt-16">
-        <div
-          id="main-content"
-          class="relative mx-auto h-full w-full max-w-screen-2xl overflow-y-auto bg-gray-50"
-        >
-          <main>
-            <router-outlet />
-          </main>
-        </div>
-      </div>
-    } @else {
-      <router-outlet />
-    }`,
+  templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
   isAuthenticated = true;
